c991ac7e2eed6b68669c5006db956eccc3c922dd
Ian Mayton <ian.mayton@gmail.com>
2020-07-10 23:11:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,11 +57,7 @@ function hashTwoSum(array, sum)
     let numOne = sortedArray[i]
     let missingNum = sum - sortedArray[i]
     console.log(hash.missingNum);
-    if(sortedArray[i] > sum - 1)
-    {
-      sortedArray.splice(i, array.length)
-    }
-    else if(hash[`${missingNum}`])
+    if(sortedArray[i] < sum && hash[`${missingNum}`])
     {
       solutions.push([sortedArray[i], missingNum])
       sortedArray.splice(i, 1)
